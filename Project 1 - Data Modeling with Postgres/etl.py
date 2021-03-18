e5c152cff6f8baca23b06a27c07999d57b84518f
Felipe Cosse <felipepaoo@gmail.com>
2021-03-18 20:23:53 -0300

    Alter python etl.py

--- Project 1 - Data Modeling with Postgres/etl.py
+++ Project 1 - Data Modeling with Postgres/etl.py
@@ -85,7 +85,7 @@ def process_log_file(cur, filepath):
     user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
 
     # insert user records
-    for i, row in user_df.iterrows():
+    for _, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
 
     # insert songplay records
